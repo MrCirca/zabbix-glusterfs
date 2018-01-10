c215cecea88d9aabeb9a2fc8d1e865f8f0ee608e
MrCirca <geortsir3@gmail.com>
2018-01-10 20:20:54 +0200

    Create script

---
+++ gstatus_discovery.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import json
+import subprocess
+import sys
+
+gluster_volume_names = []
+gstatus_output = subprocess.check_output('python /root/gstatus/gstatus.py -a -o json ', shell=True).decode()
+date, time, json_part = gstatus_output.split(maxsplit=2)
+gluster_info = json.loads(json_part)
+volume_list = gluster_info["volume_summary"]
+
+nargs = len(sys.argv)
+
+if nargs == 1:
+    for volume in volume_list:
+        gluster_volume_names.append({"{#VOLUME_NAME}": volume["volume_name"]})
+    print(json.dumps({'data': gluster_volume_names}))
+
+elif nargs == 2:
+    print(gluster_info[sys.argv[1]])
+elif nargs == 3:
+    for volume in volume_list:
+        if volume.get('volume_name') and sys.argv[2] == volume["volume_name"]:
+            print(volume[sys.argv[1]])
+            break
+    else:
+        if sys.argv[1] == "state":
+            print('down')
+        else:
+            print()
+        
+else:
+    print('Wrong arguments')
+
